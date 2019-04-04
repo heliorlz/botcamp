4b9dbd79226565967d59358452d437fb04104cc1
Helio Alves <heleralvess@gmail.com>
2019-04-04 14:37:30 -0300

    desafio intermediario aula 01 - CSS logo Botcamp

--- app/src/components/LogoBotcamp/index.js
+++ app/src/components/LogoBotcamp/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LogoBotcampImage from "./../../img/botcamp.png";
+import "./style.css";
 
 const LogoBotcamp = () => {
   return <img class="logo-botcamp" src={LogoBotcampImage} alt="Botcamp Logo" />;
