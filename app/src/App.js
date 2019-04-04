38d5484eeb95345f3b064e129a2ffac6f1ba0eda
Helio Alves <heleralvess@gmail.com>
2019-04-04 14:30:31 -0300

    desafio basico aula 01 - adicionar HTML logo Botcamp

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import BtnBotcamp from "./components/BtnBotcamp";
+import LogoBotcamp from "./components/LogoBotcamp";
 
-class App extends Component {
-  render() {
-    return <BtnBotcamp />
-  }
-}
+const App = () => {
+  return [<BtnBotcamp />, <LogoBotcamp />];
+};
 
 export default App;
---
+++ app/src/components/LogoBotcamp/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LogoBotcampImage from "./../../img/botcamp.png";
+
+const LogoBotcamp = () => {
+  return <img class="logo-botcamp" src={LogoBotcampImage} alt="Botcamp Logo" />;
+};
+
+export default LogoBotcamp;
