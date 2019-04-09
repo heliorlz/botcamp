790d655122afa04a6a5134c5855a0a06d060f774
Helio Alves <heleralvess@gmail.com>
2019-04-09 13:30:09 -0300

    desafio basico aula 04 - adicionando input no chat

---
+++ app/src/components/InputBotcamp/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const InputBotcamp = styled.input.attrs({
+    placeholder: "Diz aí..."
+})`
+  width: 100%;
+  height: 50px;
+  padding: 13px 16px;
+  box-sizing: border-box;
+  background-color: #fbfbff;
+  font-size: 20px;
+`;
+
+export default InputBotcamp;
--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -3,12 +3,19 @@ import React from "react";
 import LogoBotcamp from "../../components/LogoBotcamp";
 import LogoutBotcamp from "../../components/LogoutBotcamp";
 import HeaderWrapper from "../../components/HeaderWrapper";
+import FooterWrapper from "../../components/FooterWraper";
+import InputBotcamp from "../../components/InputBotcamp";
 
 const Chat = () => (
-  <HeaderWrapper>
-    <LogoBotcamp small />
-    <LogoutBotcamp />
-  </HeaderWrapper>
+  <>
+    <HeaderWrapper>
+      <LogoBotcamp small />
+      <LogoutBotcamp />
+    </HeaderWrapper>
+    <FooterWrapper>
+      <InputBotcamp />
+    </FooterWrapper>
+  </>
 );
 
 export default Chat;
