95daf75e1c62a8f6c245b67996ac704e3baa3e61
Helio Alves <heleralvess@gmail.com>
2019-04-11 08:37:19 -0300

    desafio intermediario: inserindo diagonal no HeaderWrapper

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -4,7 +4,26 @@ const HeaderWrapper = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  height: 70px;
+  box-sizing: border-box;
   padding: 10px 16px;
-  background-color: #26265e;
+  background-color: transparent;
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100vw;
+    height: 100%;
+    background-color: #26265e;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    transform-origin: bottom right;
+    transform: skewY(2deg);
+  }
 `;
+
 export default HeaderWrapper;
--- app/src/components/LogoBotcamp/index.js
+++ app/src/components/LogoBotcamp/index.js
@@ -5,7 +5,8 @@ const LogoBotcamp = styled.img.attrs({
   src: logoBotcampImage,
   alt: "Botcamp Logo"
 })`
-  width: ${props => props.small ? "200px" : "248px"};
+  width: ${props => (props.small ? "200px" : "248px")};
+  margin-bottom: ${props => (props.small ? "12px" : "0")};
 `;
 
 export default LogoBotcamp;
